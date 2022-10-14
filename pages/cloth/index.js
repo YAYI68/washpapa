7a100e41a8015abc24efed34dc7a2f7186f691f1
Yayi Abiodun <58443752+YAYI68@users.noreply.github.com>
2022-10-15 00:10:31 +0100

    commit

--- pages/cloth/index.js
+++ pages/cloth/index.js
@@ -1,16 +1,30 @@
+// import path from "path";
+// import fs from "fs/promises";
 import React, { Fragment,useEffect,useRef,useState } from 'react'
 import Main from '../../components/Main'
 import ProductContainer from '../../components/ProductContainer'
 import { clothData } from '../../data/dummy'
 
 
-function Cloth() {
-
+function Cloth(props) {
+   const { cloth } = props
   return (
     <Main className='w-full mt-[12vh] '>
-    <ProductContainer products={clothData} />
+    <ProductContainer products={cloth} />
     </Main>
   )
+}
+
+
+export async function getStaticProps (){
+    // const filepath = path.join(process.cwd(), 'data','cloth.json')
+    // const jsonData = await fs.readFile(filepath)
+    // const data = JSON.parse(jsonData)
+  return{
+    props:{
+       cloth:clothData
+    }
+  }
 }
 
 export default Cloth
