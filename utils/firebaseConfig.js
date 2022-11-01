73da1748c9816059a4ec152bf2803a5a383eb916
Yayi Abiodun <58443752+YAYI68@users.noreply.github.com>
2022-11-01 12:53:33 +0100

    commit

---
+++ utils/firebaseConfig.js
@@ -0,0 +1,19 @@
+
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC4EAYDke8L6aojUovOmp0HofBt8vb4dEU",
+  authDomain: "wash-papa.firebaseapp.com",
+  databaseURL: "https://wash-papa-default-rtdb.firebaseio.com",
+  projectId: "wash-papa",
+  storageBucket: "wash-papa.appspot.com",
+  messagingSenderId: "926525150262",
+  appId: "1:926525150262:web:dd6972477592d817f1f497"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app)
+export const storage = getStorage(app)
