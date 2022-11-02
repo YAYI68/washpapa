7069967049ab4e4044166abfc5469b5e84e6c1fa
Yayi Abiodun <58443752+YAYI68@users.noreply.github.com>
2022-11-02 16:45:09 +0100

    commit

--- db/auth.js
+++ db/auth.js
@@ -1,61 +1,37 @@
 import { auth, db } from "../config/firebaseConfig";
-import { sendSignInLinkToEmail } from "firebase/auth";
+import { createUserWithEmailAndPassword, sendEmailVerification  } from "firebase/auth";
 import { ref, set } from "firebase/database";
 
 
-const actionCodeSettings = {
-    // URL you want to redirect back to. The domain (www.example.com) for this
-    // URL must be in the authorized domains list in the Firebase Console.
-    url: 'http://localhost:3000/account/login',
-    // This must be true.
-    handleCodeInApp: false,
-    // iOS: {
-    //   bundleId: 'com.example.ios'
-    // },
-    // android: {
-    //   packageName: 'com.example.android',
-    //   installApp: true,
-    //   minimumVersion: '12'
-    // },
-    // dynamicLinkDomain: 'example.page.link'
-  };
-
 export const signup = async (data)=>{
     const { email, password, phone } = data;
-    try{
-        const user = await sendSignInLinkToEmail(auth,email,actionCodeSettings)
-        localStorage.setItem('emailForSignIn', email);
-        const userRef = ref(db,`Users`);
-         await set(userRef,{
-            email,
-            phone,
-            balance,
-            userName:"",
-            balance:500,
-         })
+    console.log({email, phone})
+    try{  
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password) 
+      await  set(ref(db, 'Users/' + userCredential.user.uid), {
+        email,
+        phone,
+        userName:"",
+        balance:500,
+        uid:userCredential.user.uid,
+      });
+     await sendEmailVerification(auth.currentUser)
+       return userCredential.user     
     }
     catch(error){
-
-    }
-
-
-
-       
-
+        return error;
+    }    
 }
 
 
 
 export const signIn = async(data)=>{
-    const { email, password, phoneNumber } = data
-
+    const { email, password, phoneNumber } = data;
     try{
-
         if(!email || !email.includes("@") || 
         !password || password.trim().length <  7){
             res.status(422).json({ message:"Invalid email or password"})
         }
-
 
     }
     catch(error){
--- pages/account/register.js
+++ pages/account/register.js
@@ -22,17 +22,13 @@ const Register = () => {
    const email = emailRef.current.value
    const phone = phoneRef.current.value
    const password = passwordRef.current.value
-    await signup({email, phone, password})
+   const user =  await signup({email, phone, password})
+   //  setTimeout(() => {
+   //     setErrorMessage("Incorrect email / password . Try login with a valid email and correct password")
+   //  }, 500);
 
-    console.log({email, phone, password})
-
-
-    setTimeout(() => {
-       setErrorMessage("Incorrect email / password . Try login with a valid email and correct password")
-    }, 500);
-
-
-    console.log(" User registered successfully")
+     console.log({user})
+   //  console.log(" User registered successfully")
   }
     const emailBlur = ()=>{
        if(emailRef.current.value === ""){
@@ -73,9 +69,8 @@ const Register = () => {
                        <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                         <svg className="w-5 h-5 text-blue-500 dark:text-blue-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path></svg>
                        </div>
-                       <input ref={phoneRef} type="tel" id="phone_number" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" className="bg-gray-50 border focus:outline-light-blue border-gray-300 text-gray-700 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="phone number" />
+                       <input ref={phoneRef} type="text" id="phone_number"  className="bg-gray-50 border focus:outline-light-blue border-gray-300 text-gray-700 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="phone number" />
                        </div>
-                     
                     </div>
                     <div className='flex flex-col gap-1 w-full'>
                     <label htmlFor="password" className='ml-2'>Password</label>
