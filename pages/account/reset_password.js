1043f7094ca535b831299182833e161a495b3689
Yayi Abiodun <58443752+YAYI68@users.noreply.github.com>
2023-06-08 21:53:00 +0100

    commit

--- pages/account/reset_password.js
+++ pages/account/reset_password.js
@@ -5,6 +5,7 @@ import Main from '../../components/Main'
 import { resetPassword } from '../../db/auth';
 import { authvalidator } from '../../utils/validator';
 import { toast } from 'react-hot-toast';
+import { Spinner } from '../../components/Spinner';
 
 
 
